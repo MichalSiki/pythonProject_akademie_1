0a239d181b3c4bed441067197cb93b38d421ad81
Michal <sikora1.michal1@google.com>
2022-02-20 20:48:51 +0100

    Uprava souboru

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-username = input("Zadej heslo: ")
+uzivatele = {"bob", "ann", "mike", "liz"}
+password_n = {"123", "pass123", "password123", "pass123"}
+
+username = input("Zadej jmeno: ")
 password = input("Zadej heslo: ")
 print("username: ", username)
 print("password: ", password)
+
+if username
