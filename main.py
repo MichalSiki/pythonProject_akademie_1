b3ac56c61ef0b16fcb63da5a7ecf94e9c921457f
MichalS <99993669+MichalSiki@users.noreply.github.com>
2022-02-21 09:49:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-uzivatele = {"bob", "ann", "mike", "liz"}
-password_n = {"123", "pass123", "password123", "pass123"}
+uzivatele = ("bob", "ann", "mike", "liz")
+password_n = ("123", "pass123", "password123", "pass123")
 
 username = input("Zadej jmeno: ")
 password = input("Zadej heslo: ")
 print("username: ", username)
 print("password: ", password)
 
-if username
+if username in uzivatele and password in password_n:
+    print("Jmeno a heslo je správné")
+else:
+    print("Zadej správné jméno a heslo")
